ab16fce8dc19cddb57192b64fd6445c128b97f39
Vinh Tran <s3500659@student.edu.au>
2020-07-13 13:50:59 +1000

    InitialCommit

---
+++ S3500659_A2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S3500659_A2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
