85ea3afd0788cafff9f66113c18cf08a29376743
Vinh Tran <s3500659@student.edu.au>
2020-07-15 15:39:58 +1000

    add seed data for customer, login, account, transactions

--- S3500659_A2/Startup.cs
+++ S3500659_A2/Startup.cs
@@ -34,6 +34,12 @@ namespace S3500659_A2
                 // Enable lazy loading.
                 options.UseLazyLoadingProxies();
             });
+
+            // use sessions
+            services.AddSession(options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,7 @@ namespace S3500659_A2
         {
             if (env.IsDevelopment())
             {
+                // MVC Core Error handling
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -49,12 +56,12 @@ namespace S3500659_A2
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
