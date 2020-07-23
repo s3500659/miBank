c9372ab7c9fa09c9bc78e02f5efde9dec8827c1b
Vinh Tran <s3500659@student.edu.au>
2020-07-23 15:16:31 +1000

    add statement, need to do paging

---
+++ S3500659_A2/Controllers/StatementController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using S3500659_A2.Data;
+using S3500659_A2.Models;
+
+namespace S3500659_A2.Controllers
+{
+    public class StatementController : Controller
+    {
+        private readonly A2Context _context;
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public StatementController(A2Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+
+            return View(account);
+        }
+    }
+}
